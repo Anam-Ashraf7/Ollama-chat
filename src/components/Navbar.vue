e43d1236a25883210c521b3711d9939ef42b0120
Anam-Ashraf7 <anam.ashraf.7357@gmail.com>
2024-05-04 22:37:37 +0530

    Added ollama logo

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -27,53 +27,34 @@ export default {
 
 <template>
   <!-- <div> -->
-  <div class="navbar bg-base-100">
-    <div class="flex-none">
-      <button class="btn btn-square btn-ghost">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          class="inline-block w-5 h-5 stroke-current"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M4 6h16M4 12h16M4 18h16"
-          ></path>
-        </svg>
-      </button>
-    </div>
-    <div class="flex-1">
-      <a class="btn btn-ghost text-xl">Ollama Chat</a>
+  <div class="navbar bg-base-200">
+    <div class="flex-1 items-center bg-base">
+      <div class="px-3 chat-image avatar">
+          <div class="w-10 rounded-full">
+            <img
+            class="bg-white !object-contain"
+            alt="Tailwind CSS chat bubble component"
+            src="https://ollama.com/public/ollama.png"
+            />
+        </div>
+      </div>
+      <a class="text-xl">Ollama Chat</a>
     </div>
     <div class="flex-none">
       <div class="dropdown dropdown-bottom dropdown-end mr-5">
-        <button tabindex="0" role="button" class="btn btn-square btn-ghost">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            class="inline-block w-5 h-5 stroke-current"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
-            ></path>
-          </svg>
-        </button>
+        <div tabindex="0" role="button" class="flex btn btn-ghost">
+          Models 
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-down"><path d="m6 9 6 6 6-6"/></svg>
+        </div>
     <ul
       tabindex="0"
-      class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+      class="dropdown-content z-[1] menu p-2 shadow bg-base-200 rounded-box w-52"
     >
       <li v-for="model in list" :key="model">
         <a>{{ model }}</a>
       </li>
     </ul>
-  </div>
+    </div>
     </div>
   </div>
   <!-- </div> -->
