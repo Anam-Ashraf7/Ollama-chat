310b2bf522b9f9ccf56b3961c1942e52a159c774
Alwin Sunil <64636626+AlwinSunil@users.noreply.github.com>
2024-05-04 18:49:02 +0530

    Added daisy ui

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import Navbar from "./components/Navbar.vue";
 </script>
 
 <template>
-  <div>
-  </div>
-  
+  <div></div>
+  <Navbar />
   <HelloWorld msg="Vite + Vue" />
 </template>
-
---
+++ src/components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div className="navbar bg-base-100">
+    <div className="flex-none">
+      <button className="btn btn-square btn-ghost">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          className="inline-block w-5 h-5"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            fill="#fff"
+            d="M4 6h16M4 12h16M4 18h16"
+          ></path>
+        </svg>
+      </button>
+    </div>
+    <div className="flex-1">
+      <a className="btn btn-ghost text-xl">Ollama Chat</a>
+    </div>
+  </div>
+</template>
