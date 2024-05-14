0ed04c30ac1f6c6f6d0b0a142bf3a2caa9dd53c0
mugilankani <mugilankani951@gmail.com>
2024-05-14 09:57:03 +0530

    added info button

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -23,7 +23,7 @@ export default {
       console.log(this.list);
     },
     selectModel(model) {
-      this.$emit('modelSelected', model); // Emit the event with the selected model
+      this.$emit("modelSelected", model); // Emit the event with the selected model
       this.isDropdownOpen = false; // Close the dropdown
     },
     toggleDropdown() {
@@ -36,21 +36,25 @@ export default {
   <!-- <div> -->
   <div class="navbar bg-base-100">
     <div class="flex-none">
-      <button class="btn btn-square btn-ghost">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          class="inline-block w-5 h-5 stroke-current"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M4 6h16M4 12h16M4 18h16"
-          ></path>
-        </svg>
-      </button>
+      <a
+        href="https://github.com/Anam-Ashraf7/Strix?tab=readme-ov-file#how-to-run"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <button class="btn btn-square btn-ghost">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 -960 960 960"
+            width="24px"
+            fill="#e8eaed"
+          >
+            <path
+              d="M480-280q17 0 28.5-11.5T520-320v-160q0-17-11.5-28.5T480-520q-17 0-28.5 11.5T440-480v160q0 17 11.5 28.5T480-280Zm0-320q17 0 28.5-11.5T520-640q0-17-11.5-28.5T480-680q-17 0-28.5 11.5T440-640q0 17 11.5 28.5T480-600Zm0 520q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"
+            />
+          </svg>
+        </button>
+      </a>
     </div>
     <div class="flex-1">
       <a class="btn btn-ghost text-xl">Ollama Chat</a>
